25e880b2290e5bce5afb518f954ec1d32961653e
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-02-06 21:34:47 +0330

    part27

--- src/ParentCompo.jsx
+++ src/ParentCompo.jsx
@@ -1,26 +1,48 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component, createRef, PureComponent } from "react";
 import PureCompo from "./PureComponent";
+import Fcomponent from "./Fcomponent";
 
 class ParentCompo extends Component {
-   constructor(){
-    super()
-    this.state={
-        name : "Mohammad"
+
+    constructor(){
+        super()
+        // this.componnentRef = createRef();
+        this.myInput = createRef();
     }
-   }
-   componentDidMount(){
-    setInterval(() => {
-        this.setState({
-            name : "Mohammad"+new Date()
-        })
-    }, 1000);
-   }
+
+    componentDidMount(){
+        this.myInput.current.focus();
+    }
+
+
+    // handleChangeCompoName =()=>{
+    //     this.componnentRef.current.handleChangeName();
+    // }
+
+//    constructor(){
+//     super()
+//     this.state={
+//         name : "Mohammad"
+//     }
+//    }
+//    componentDidMount(){
+//     setInterval(() => {
+//         this.setState({
+//             name : "Mohammad"+new Date()
+//         })
+//     }, 1000);
+//    }
    render(){
-    console.log("----------parent-----------");
+    // console.log("----------parent-----------");
+    // console.log(this.componnentRef);
     
     return(
         <div>
-            <PureCompo name={this.state.name}/>
+            {/* روش 1: برای کامپوننت نوع کلاسی روش زیر می باشد */}
+            {/* <PureCompo name={this.state.name}/> */}
+            {/* <PureCompo ref={this.componnentRef}/> */}
+            {/* <button className="btn btn-info" onClick={this.handleChangeCompoName}>Test</button> */}
+            <Fcomponent ref={this.myInput}/>            
         </div>
     )
    }
